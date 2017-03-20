1036e059cc52e89f8a6f894ac5671a5266c8ad84
Osher Cohen <osher.y.cohen@gmail.com>
2017-03-20 16:28:51 -0400

    Shortened the cart angular code by using closures.

--- zuul/src/main/resources/static/scripts/CartController.js
+++ zuul/src/main/resources/static/scripts/CartController.js
@@ -1,28 +1,26 @@
-angular.module('MainApp').controller('CartController', function ($scope, $http, $rootScope, $log) {
+angular.module('MainApp').controller('CartController', function ($scope, $http, $rootScope) {
     $scope.cartItems = [];
     $http({
         url: '/cart/carts/search/findByCustomerId?customerId=' + $rootScope.customer.customerId,
         method: 'GET'
-    }).then(function(response){
-        $scope.cart = response.data;
-        $log.info("Here is your cart");
-        $log.info($scope.cart);
-    });
-    for (let productId in $scope.cart.items) {
-        $http({
-            url: '/product/products/' + productId,
-            method: 'GET'
-        }).then(function (response) {
-            $scope.cartItems.push({
-                "productId": response.data.productId,
-                "productName": response.data.productName,
-                "price": response.data.price,
-                "information": response.data.information,
-                "description": response.data.description,
-                "manufacturer": response.data.manufacturer,
-                "productImage": response.data.productImage,
-                "quantity": $scope.cart.items[response.data.productId]
+    }).then(function (response) {
+        let cart = response.data;
+        for (let productId in cart.items) {
+            $http({
+                url: '/product/products/' + productId,
+                method: 'GET'
+            }).then(function (response) {
+                $scope.cartItems.push({
+                    "productId": response.data.productId,
+                    "productName": response.data.productName,
+                    "price": response.data.price,
+                    "information": response.data.information,
+                    "description": response.data.description,
+                    "manufacturer": response.data.manufacturer,
+                    "productImage": response.data.productImage,
+                    "quantity": cart.items[response.data.productId]
+                });
             });
-        });
-    }
+        }
+    });
 });
